679028f66781140b4dba2d14a02043886c144312
Niels Mouthaan <nielsmouthaan@me.com>
2023-11-20 11:27:51 +0100

    Initial Commit

---
+++ Backup Status/Backup_StatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Backup_StatusApp.swift
+//  Backup Status
+//
+//  Created by Niels Mouthaan on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Backup_StatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Backup Status/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Backup Status
+//
+//  Created by Niels Mouthaan on 20/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
